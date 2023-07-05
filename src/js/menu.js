a20a43997e5cc9cd4dd931ca3372f804c995658e
Baranovska29 <131688923+Baranovska29@users.noreply.github.com>
2023-07-05 14:44:21 +0300

    header

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,16 @@
 (() => {
     const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
+      openMenuBtn: document.querySelector("[data-memu-open]"),
       closeMenuBtn: document.querySelector("[data-menu-close]"),
       menu: document.querySelector("[data-menu]"),
     };
   
     refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.openMenuBtn2.addEventListener("click", toggleMenu);
     refs.closeMenuBtn.addEventListener("click", toggleMenu);
   
-    function toggleMenu() {
+    function toggleModal() {
       refs.menu.classList.toggle("is-hidden");
-      document.body.classList.toggle('no-scroll')
+      refs.menu.classList.toggle("no-scroll");
     }
   })();
