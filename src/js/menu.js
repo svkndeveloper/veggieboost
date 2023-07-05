17b04d24c55ebfa9a4b77843323e82b432cb4195
Baranovska29 <131688923+Baranovska29@users.noreply.github.com>
2023-07-05 22:40:26 +0300

    modal

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,6 +5,7 @@
       closeMenuBtn1: document.querySelector("[data-menu-close1]"),
       closeMenuBtn2: document.querySelector("[data-menu-close2]"),
       closeMenuBtn3: document.querySelector("[data-menu-close3]"),
+      closeMenuBtn4: document.querySelector("[data-menu-close4]"),
       menu: document.querySelector("[data-menu]"),
     };
   
@@ -13,6 +14,7 @@
     refs.closeMenuBtn1.addEventListener("click", toggleMenu);
     refs.closeMenuBtn2.addEventListener("click", toggleMenu);
     refs.closeMenuBtn3.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn4.addEventListener("click", toggleMenu);
   
     function toggleMenu() {
       refs.menu.classList.toggle("is-hidden");
