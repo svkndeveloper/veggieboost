ed5ea572155b6ff05d4ac62bb3b043c59e64e81a
Baranovska29 <131688923+Baranovska29@users.noreply.github.com>
2023-07-05 20:48:45 +0300

    close

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,14 +2,20 @@
     const refs = {
       openMenuBtn: document.querySelector("[data-menu-open]"),
       closeMenuBtn: document.querySelector("[data-menu-close]"),
+      closeMenuBtn1: document.querySelector("[data-menu-close1]"),
+      closeMenuBtn2: document.querySelector("[data-menu-close2]"),
+      closeMenuBtn3: document.querySelector("[data-menu-close3]"),
       menu: document.querySelector("[data-menu]"),
     };
   
     refs.openMenuBtn.addEventListener("click", toggleMenu);
     refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn1.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn2.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn3.addEventListener("click", toggleMenu);
   
     function toggleMenu() {
       refs.menu.classList.toggle("is-hidden");
-      refs.menu.classList.toggle("no-scroll");
+      document.body.classList.toggle("no-scroll");
     }
   })();
