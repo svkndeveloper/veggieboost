acdb8ae107740a647de7bcb1fa409816410d93c6
Mac <mac@Macs-Mac-mini.local>
2023-12-04 12:10:22 +0200

    change build name

--- src/js/btnToTop.js
+++ src/js/btnToTop.js
@@ -2,7 +2,7 @@ const myButton = document.getElementById("myBtn");
 myButton.addEventListener('click', onPageTop);
 
 window.onscroll = function () {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+  if (document.body.scrollTop > 380 || document.documentElement.scrollTop > 380) {
     myButton.style.display = "block";
   } else {
     myButton.style.display = "none";
